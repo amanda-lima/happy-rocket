21bf0b95cc39227367cdd8940c34e35bb169679e
Amanda Lima <amanda.limaa0202@gmail.com>
2020-10-15 20:22:40 -0300

    segundo dia

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,49 @@
+// create map
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+const map = L.map('mapid', options).setView([-27.2151086,-49.6527344], 13);
+
+// create and add TileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+.addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+//create and add marker
+L.marker([-27.2151086,-49.6527344],{ icon })
+    .addTo(map)
+
+
+
+    
+/* image gallery */
+function selectImage(event) {
+    const button = event.currentTarget
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+    //selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+    //atualizar o container de imagem
+    imageContainer.src = image.src
+    //adicionar a classe .active para este botão
+    button.classList.add('active')
+}
